98b23476d9db8ab54a8394a4a1ba47e144c13819
PedroManzanares14 <00230422@uca.edu.sv>
2023-04-25 19:55:15 -0600

    a

---
+++ excepcionesejm/src/excepcionesejm/Excepcionesejm.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package excepcionesejm;
+
+import java.util.AbstractCollection.ImputMismatchExeption;
+import java.util.AbstractCollection.Scanner;
+
+
+class SiMeGusta extends Exception{
+
+}
+
+class NoMeGusta{
+
+}
+
+/**
+ *
+ * @author pedro
+ */
+
+
+public class Excepcionesejm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
